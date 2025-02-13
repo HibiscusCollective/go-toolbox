c6ebee683d99d15f35174e3ab02241192d6ecd48
pfouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-02-13 19:45:37 +0800

    feat(fxmap): return an error when failing to write to the hash function

--- fxmap/map.go
+++ fxmap/map.go
@@ -1,5 +1,80 @@
 package fxmap
 
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"hash"
+	"hash/fnv"
+	"reflect"
+)
+
+type Hashable interface {
+	Hash(fn hash.Hash) []byte
+}
+
+type Byteable interface {
+	Bytes() []byte
+}
+
+type HashError interface {
+	error
+	HashFn() HashFunctionDescription
+}
+
+type HashMap[K, V any, SZ hashSize] struct {
+	hash hash.Hash
+}
+
+type HashFunctionDescription struct {
+	Algorithm string
+	Size      int
+	BlockSize int
+}
+
+type hashSize interface {
+	uint32
+}
+
+type hashComparable interface {
+	comparable
+}
+
+type pair[K, V any] struct {
+	key K
+	val V
+}
+
+type hashError struct {
+	error
+	hashFn HashFunctionDescription
+}
+
+func New[K hashComparable, V any](m map[K]V) (HashMap[K, V, uint32], error) {
+	return NewWith32bitHashFunction(m, fnv.New32a())
+}
+
+func NewWith32bitHashFunction[K hashComparable, V any](m map[K]V, fn hash.Hash32) (HashMap[K, V, uint32], error) {
+	hm := HashMap[K, V, uint32]{
+		hash: fn,
+	}
+
+	for k := range m {
+		var buf bytes.Buffer
+		enc := gob.NewEncoder(&buf)
+		if err := enc.Encode(k); err != nil {
+			panic(err)
+		}
+
+		_, err := fn.Write(buf.Bytes())
+		if err != nil {
+			return HashMap[K, V, uint32]{}, fmtHashError(fn, "failed to compute hash: %w", err)
+		}
+	}
+
+	return hm, nil
+}
+
 func Invert[K, V comparable](m map[K]V) map[V]K {
 	if m == nil {
 		return nil
@@ -12,3 +87,16 @@ func Invert[K, V comparable](m map[K]V) map[V]K {
 
 	return r
 }
+
+func fmtHashError(fn hash.Hash, str string, args ...any) hashError {
+	return hashError{
+		error: fmt.Errorf(str, args...),
+		hashFn: HashFunctionDescription{
+			Algorithm: reflect.TypeOf(fn).String(),
+			Size:      fn.Size() * 8,
+			BlockSize: fn.BlockSize() * 8,
+		},
+	}
+}
+
+func (e hashError) HashFn() HashFunctionDescription { return e.hashFn }
--- fxmap/map_test.go
+++ fxmap/map_test.go
@@ -1,6 +1,9 @@
 package fxmap_test
 
 import (
+	"errors"
+	"hash"
+	"hash/crc32"
 	"testing"
 
 	"github.com/HibiscusCollective/go-toolbox/fxmap"
@@ -8,6 +11,36 @@ import (
 )
 
 type test func(t gomega.Gomega)
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]test{
+		"should return an error if the hash function fails": func(g gomega.Gomega) {
+			_, err := fxmap.NewWith32bitHashFunction(
+				map[int32]string{1: "one"},
+				newBrokenHash32Fn("boom"),
+			)
+
+			var herr fxmap.HashError
+			g.Expect(err).To(gomega.MatchError(func(err error) bool {
+				return errors.As(err, &herr)
+			}, "fxmap.HashError"))
+
+			g.Expect(herr.HashFn().Algorithm).To(gomega.Equal("fxmap_test.brokenHash32Fn"))
+			g.Expect(herr.HashFn().Size).To(gomega.Equal(32))
+			g.Expect(herr.HashFn().BlockSize).To(gomega.Equal(4))
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			test(gomega.NewWithT(t))
+		})
+	}
+}
 
 func TestInvert(t *testing.T) {
 	t.Parallel()
@@ -26,11 +59,31 @@ func TestInvert(t *testing.T) {
 		"should return a map with the values in place of the keys": func(g gomega.Gomega) {
 			got := fxmap.Invert(map[int]string{1: "one", 2: "two"})
 
-			g.Expect(got).To(gomega.Equal(map[string]int{"one": 1, "two": 2}))
+			g.Expect(got).Should(gomega.Equal(map[string]int{"one": 1, "two": 2}))
 		},
 	}
 
 	for name, test := range tests {
-		t.Run(name, func(t *testing.T) { test(gomega.NewWithT(t)) })
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			test(gomega.NewWithT(t))
+		})
 	}
+}
+
+type brokenHash32Fn struct {
+	hash.Hash32
+	err error
+}
+
+func newBrokenHash32Fn(err string) brokenHash32Fn {
+	return brokenHash32Fn{
+		Hash32: crc32.New(crc32.IEEETable),
+		err:    errors.New(err),
+	}
+}
+
+func (h brokenHash32Fn) Write(data []byte) (int, error) {
+	return 0, h.err
 }
