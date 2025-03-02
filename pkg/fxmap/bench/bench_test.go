97e1d5941cdee17c29ab828f6267ddcba9c240bd
pfouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-03-02 16:18:51 +0800

    refactor(pkg): move libraries into a dedicated pkg folder

---
+++ pkg/fxmap/bench/bench_test.go
@@ -0,0 +1,47 @@
+package bench
+
+import (
+	"testing"
+
+	"github.com/HibiscusCollective/go-toolbox/fxmap/pkg/hashmap"
+	"github.com/jaswdr/faker/v2"
+)
+
+func BenchmarkInvert(b *testing.B) {
+	fake := faker.New()
+
+	testData := generateTestData(4, 16, generateFakesMap(fake.Int64, fake.Lorem().Word))
+
+	for name, td := range testData {
+		b.Run(name+": control", RunInvertControlBenchmark(td))
+		b.Run(name+": experiment", RunInvertExperimentBenchmark(td))
+	}
+
+}
+
+func RunInvertControlBenchmark(items map[int64]string) func(b *testing.B) {
+	return func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			b.StartTimer()
+			inverted := make(map[string]int64)
+			for key, val := range items {
+				inverted[val] = key
+			}
+			b.StopTimer()
+		}
+
+		b.ReportAllocs()
+	}
+}
+
+func RunInvertExperimentBenchmark(items map[int64]string) func(b *testing.B) {
+	return func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			b.StartTimer()
+			hashmap.Invert(items)
+			b.StopTimer()
+		}
+
+		b.ReportAllocs()
+	}
+}
---
+++ pkg/fxmap/go.mod
@@ -0,0 +1,16 @@
+module github.com/HibiscusCollective/go-toolbox/pkg/fxmap
+
+go 1.23
+
+require (
+	github.com/gofrs/uuid/v5 v5.3.1
+	github.com/jaswdr/faker/v2 v2.3.3
+	github.com/onsi/gomega v1.36.2
+)
+
+require (
+	github.com/google/go-cmp v0.6.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
