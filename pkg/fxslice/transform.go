c266bc0d03392a0e2b82b68bd420706ac44f8016
pfouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-03-03 23:12:29 +0800

    feat(fxslice): implement cast function to cast a slice of one type to another

--- pkg/fxslice/transform.go
+++ pkg/fxslice/transform.go
@@ -1,12 +1,92 @@
+// Package fxslice provides functions to extend the slices package in the standard library
 package fxslice
 
-// Transform transforms a slice of type T to a slice of type R
-func Transform[T, R any](src []T, f func(T) R) []R {
-	xf := make([]R, len(src))
+import (
+	"errors"
+	"fmt"
+	"reflect"
+)
 
-	for i, v := range src {
-		xf[i] = f(v)
+// Transformer is a function that transforms a value of type T to a value of type R
+type Transformer[T, R any] func(T) R
+
+// TryTransformer is a function that transforms a value of type T to a value of type R but may return an error if it fails
+type TryTransformer[T, R any] func(T) (R, error)
+
+// Cast casts a slice of type T to a slice of type R
+func Cast[T, R any](srcs []T) ([]R, error) {
+	return TryTransform(srcs, func(src T) (R, error) {
+		dstType := reflect.TypeOf(new(R)).Elem()
+		v := reflect.ValueOf(src)
+		if !v.IsValid() || !v.CanConvert(dstType) {
+			return *new(R), NewCastError(src, dstType)
+		}
+
+		return v.Convert(dstType).Interface().(R), nil
+	})
+}
+
+// Transform transforms a slice of type T to a slice of type R
+func Transform[T, R any](src []T, fn Transformer[T, R]) []R {
+	rs, _ := TryTransform(src, succeed(fn))
+
+	return rs
+}
+
+// TryTransform transforms a slice of type T to a slice of type R
+func TryTransform[T, R any](src []T, fn TryTransformer[T, R]) ([]R, error) {
+	if src == nil || len(src) == 0 {
+		return nil, nil
 	}
 
-	return xf
+	var out []R
+	var errs []error
+
+	for i, elem := range src {
+		r, err := fn(elem)
+		if err != nil {
+			errs = append(errs, fmt.Errorf("index %d: %w", i, err))
+		} else {
+			out = append(out, r)
+		}
+	}
+
+	if len(out) == 0 {
+		return nil, errors.Join(errs...)
+	}
+
+	return out, errors.Join(errs...)
+}
+
+func succeed[T, R any](fn Transformer[T, R]) func(T) (R, error) {
+	return func(t T) (R, error) {
+		return fn(t), nil
+	}
+}
+
+// CastError is an error that indicates that type casting failed
+type CastError[T any] interface {
+	error
+	Src() T
+	Dst() reflect.Type
+}
+
+type castError[T any] struct {
+	src T
+	dst reflect.Type
+}
+
+func NewCastError[T any](src T, dst reflect.Type) castError[T] {
+	return castError[T]{src: src, dst: dst}
+}
+
+// Src returns the source value that caused the error.
+func (c castError[T]) Src() T { return c.src }
+
+// Dst returns the destination type that the source value could not be cast to.
+func (c castError[T]) Dst() reflect.Type { return c.dst }
+
+// Error implements error.
+func (c castError[T]) Error() string {
+	return fmt.Sprintf("cannot cast %[1]v of type %[1]T to %[2]s", c.src, c.dst)
 }
--- pkg/fxslice/transform_test.go
+++ pkg/fxslice/transform_test.go
@@ -1,6 +1,9 @@
 package fxslice_test
 
 import (
+	"errors"
+	"fmt"
+	"reflect"
 	"testing"
 
 	"github.com/onsi/gomega"
@@ -8,15 +11,36 @@ import (
 	"github.com/HibiscusCollective/go-toolbox/pkg/fxslice"
 )
 
-func TestTransform(t *testing.T) {
+func TestCast(t *testing.T) {
 	t.Parallel()
 
 	scns := map[string]func(t testing.TB, g gomega.Gomega){
+		"should return a nil slice if the slice is nil": func(t testing.TB, g gomega.Gomega) {
+			rs, err := fxslice.Cast[uint8, uint](nil)
+
+			g.Expect(err).To(gomega.BeNil())
+			g.Expect(rs).To(gomega.BeNil())
+		},
 		"should return an empty slice if the slice is empty": func(t testing.TB, g gomega.Gomega) {
-			g.Expect(fxslice.Transform([]uint8{}, plusTwo)).To(gomega.Equal([]uint8{}))
+			rs, err := fxslice.Cast[uint8, uint]([]uint8{})
+
+			g.Expect(err).To(gomega.BeNil())
+			g.Expect(rs).To(gomega.BeNil())
 		},
 		"should return a transformed slice": func(t testing.TB, g gomega.Gomega) {
-			g.Expect(fxslice.Transform([]uint8{1, 2, 3}, plusTwo)).To(gomega.Equal([]uint8{3, 4, 5}))
+			rs, err := fxslice.Cast[uint8, uint]([]uint8{1, 2, 3})
+
+			g.Expect(err).To(gomega.BeNil())
+			g.Expect(rs).To(gomega.Equal([]uint{1, 2, 3}))
+		},
+		"should return all errors that occurred during the transformation": func(t testing.TB, g gomega.Gomega) {
+			rs, err := fxslice.Cast[any, uint]([]any{"abc", 1, "ghi"})
+
+			g.Expect(err).To(gomega.MatchError(errors.Join(
+				fmt.Errorf("index 0: %w", fxslice.NewCastError(any("abc"), reflect.TypeOf(uint(0)))),
+				fmt.Errorf("index 2: %w", fxslice.NewCastError(any("ghi"), reflect.TypeOf(uint(0)))),
+			)))
+			g.Expect(rs).To(gomega.Equal([]uint{1}))
 		},
 	}
 
@@ -27,9 +51,88 @@ func TestTransform(t *testing.T) {
 			test(t, gomega.NewWithT(t))
 		})
 	}
+}
 
+func TestTransform(t *testing.T) {
+	t.Parallel()
+
+	scns := map[string]func(t testing.TB, g gomega.Gomega){
+		"should return a nil slice if the slice is nil": func(t testing.TB, g gomega.Gomega) {
+			rs := fxslice.Transform(nil, plusTwo)
+			g.Expect(rs).To(gomega.BeNil())
+		},
+		"should return an empty slice if the slice is empty": func(t testing.TB, g gomega.Gomega) {
+			rs := fxslice.Transform([]uint8{}, plusTwo)
+
+			g.Expect(rs).To(gomega.Equal([]uint8{}))
+		},
+		"should return a transformed slice": func(t testing.TB, g gomega.Gomega) {
+			rs := fxslice.Transform([]uint8{1, 2, 3}, plusTwo)
+
+			g.Expect(rs).To(gomega.Equal([]uint8{3, 4, 5}))
+		},
+	}
+
+	for name, test := range scns {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			test(t, gomega.NewWithT(t))
+		})
+	}
+}
+
+func TestTryTransform(t *testing.T) {
+	t.Parallel()
+
+	scns := map[string]func(t testing.TB, g gomega.Gomega){
+		"should return a nil slice if the slice is nil": func(t testing.TB, g gomega.Gomega) {
+			rs, err := fxslice.TryTransform(nil, succeed(plusTwo))
+
+			g.Expect(err).To(gomega.BeNil())
+			g.Expect(rs).To(gomega.BeNil())
+		},
+		"should return a nil slice if the slice is empty": func(t testing.TB, g gomega.Gomega) {
+			rs, err := fxslice.TryTransform([]uint8{}, succeed(plusTwo))
+
+			g.Expect(err).To(gomega.BeNil())
+			g.Expect(rs).To(gomega.BeNil())
+		},
+		"should return a transformed slice": func(t testing.TB, g gomega.Gomega) {
+			rs, err := fxslice.TryTransform([]uint8{1, 2, 3}, succeed(plusTwo))
+
+			g.Expect(err).To(gomega.BeNil())
+			g.Expect(rs).To(gomega.Equal([]uint8{3, 4, 5}))
+		},
+		"should return all errors that occurred during the transformation": func(t testing.TB, g gomega.Gomega) {
+			rs, err := fxslice.TryTransform([]uint8{1, 2, 3}, fail(plusTwo))
+
+			g.Expect(err).ToNot(gomega.Equal(errors.Join(errors.New("failed"), errors.New("failed"), errors.New("failed"))))
+			g.Expect(rs).To(gomega.BeNil())
+		},
+	}
+
+	for name, test := range scns {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			test(t, gomega.NewWithT(t))
+		})
+	}
 }
 
 func plusTwo(it uint8) uint8 {
 	return it + 2
+}
+
+func succeed[T, R any](fn fxslice.Transformer[T, R]) func(T) (R, error) {
+	return func(t T) (R, error) {
+		return fn(t), nil
+	}
+}
+
+func fail[T, R any](_ fxslice.Transformer[T, R]) func(T) (R, error) {
+	return func(t T) (R, error) {
+		return *new(R), errors.New("failed")
+	}
 }
