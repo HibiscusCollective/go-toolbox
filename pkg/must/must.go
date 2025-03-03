2e56527f0ddf317a4051a9a441d619c5a4b5d69d
pfouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-03-03 20:42:26 +0800

    feat(must): implement must succeed or fail test

--- pkg/must/must.go
+++ pkg/must/must.go
@@ -1,10 +1,41 @@
 // Package must provides a helper function to handle error by panicking
 package must
 
-// OrPanic panics if the error is not nil
-func OrPanic[T any](v T, err error) T {
-	if err != nil {
-		panic(err)
+import "testing"
+
+// Success is an interface that defines the logic for requiring the successful of a function
+type Success[T any] interface {
+	OrPanic() T
+	OrFail(t testing.TB) T
+}
+
+type result[T any] struct {
+	v   T
+	err error
+}
+
+// Succeed returns a Success that resolves to the given value and error
+func Succeed[T any](val T, err error) Success[T] {
+	return result[T]{
+		v:   val,
+		err: err,
 	}
-	return v
+}
+
+// OrPanic panics if the error is not nil
+func (r result[T]) OrPanic() T {
+	if r.err != nil {
+		panic(r.err)
+	}
+
+	return r.v
+}
+
+// OrFail fails the test if the error is not nil
+func (r result[T]) OrFail(t testing.TB) T {
+	if r.err != nil {
+		t.Fatalf("unexpected error: %v", r.err)
+	}
+
+	return r.v
 }
--- pkg/must/must_test.go
+++ pkg/must/must_test.go
@@ -2,6 +2,7 @@ package must_test
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/onsi/gomega"
@@ -14,13 +15,13 @@ func TestMustOrPanic(t *testing.T) {
 
 	scns := map[string]func(g gomega.Gomega){
 		"should not panic given a nil error": func(g gomega.Gomega) {
-			val := must.OrPanic(valOrErr("hello, world!", nil))
+			val := must.Succeed(valOrErr("hello, world!", nil)).OrPanic()
 
 			g.Expect(val).To(gomega.Equal("hello, world!"))
 		},
 		"should panic given a non-nil error": func(g gomega.Gomega) {
 			g.Expect(func() {
-				must.OrPanic(valOrErr(nil, errors.New("boom")))
+				must.Succeed(valOrErr(nil, errors.New("boom"))).OrPanic()
 			}).To(gomega.Panic())
 		},
 	}
@@ -34,6 +35,45 @@ func TestMustOrPanic(t *testing.T) {
 	}
 }
 
+func TestOrFail(t *testing.T) {
+	t.Parallel()
+
+	scns := map[string]func(t testing.TB, g gomega.Gomega){
+		"should not fail given a nil error": func(t testing.TB, g gomega.Gomega) {
+			val := must.Succeed(valOrErr("hello, world!", nil)).OrFail(t)
+
+			g.Expect(val).To(gomega.Equal("hello, world!"))
+		},
+		"should fail given a non-nil error": func(t testing.TB, g gomega.Gomega) {
+			tester := mockT(t)
+			must.Succeed(valOrErr(nil, errors.New("boom"))).OrFail(tester)
+
+			g.Expect(tester.fatal).To(gomega.Equal("unexpected error: boom"))
+		},
+	}
+
+	for name, test := range scns {
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			test(t, gomega.NewWithT(t))
+		})
+	}
+}
+
+type tm struct {
+	testing.TB
+	fatal string
+}
+
 func valOrErr(val any, err error) (any, error) {
 	return val, err
+}
+
+func mockT(t testing.TB) *tm {
+	return &tm{TB: t}
+}
+
+func (t *tm) Fatalf(format string, args ...any) {
+	t.fatal = fmt.Sprintf(format, args...)
 }
